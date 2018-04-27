fc7222bb50413c1d2ca41520ccb0a5655afe964a
MarkDino21 <markdinopelonia447@gmail.com>
2018-04-27 14:06:07 +0800

    minimized code for smooth scroll

--- js/script.js
+++ js/script.js
@@ -49,38 +49,6 @@ $(document).ready (function() {
         $("body, html").scrollTo(this.hash, this.hash);
     });
 
-    // // highlighting/activating the nav while scrolling
-    // var aChildren = $("nav li").children(); // find the a children of the list items
-    // var aArray = []; // create the empty aArray
-    // for (var i=0; i < aChildren.length; i++) {
-    //     var aChild = aChildren[i];
-    //     var ahref = $(aChild).attr("href");
-    //     aArray.push(ahref);
-    // } // this for loop fills the aArray with attribute href values
-
-    // $(window).scroll(function(){
-    //     var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
-    //     var windowHeight = $(window).height(); // get the height of the window
-    //     var docHeight = $(document).height();
-    //
-    //     for (var i=0; i < aArray.length; i++) {
-    //         var theID = aArray[i];
-    //         var divPos = $(theID).offset().top; // get the offset of the div from the top of page
-    //         var divHeight = $(theID).height(); // get the height of the div in question
-    //         if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
-    //             $("a[href='" + theID + "']").addClass("nav-active");
-    //         } else {
-    //             $("a[href='" + theID + "']").removeClass("nav-active");
-    //         }
-    //     }
-    //
-    //     if(windowPos + windowHeight == docHeight) {
-    //         if (!$("nav li:last-child a").hasClass("nav-active")) {
-    //             var navActiveCurrent = $(".nav-active").attr("href");
-    //             $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
-    //             $("nav li:last-child a").addClass("nav-active");
-    //         }
-    //     }
-    // });
+    
 
 });
